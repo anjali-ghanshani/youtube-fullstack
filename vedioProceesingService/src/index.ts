1a3c5ba1dcb05d89ae41d832ae37e9d05e4587fb
anjali-ghanshani <anjali.ghanshani@gmail.com>
2025-04-08 19:25:26 +1000

    initiating vedio processing service

--- vedioProceesingService/src/index.ts
+++ vedioProceesingService/src/index.ts
@@ -1,14 +1,13 @@
 import express = require("express");
 
-const app =  express();
+const app = express();
 
 const PORT = 3000;
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Video Processing service is running on port ${PORT}`);
 });
